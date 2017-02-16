b3e2b86c8832bae8ad1b18204b0054770bb9f6c9
rstenberg <ryanstenberg10@gmail.com>
2017-02-16 14:51:25 -0800

    Fixed off by one error on clock counters

--- lab3/src/clock.v
+++ lab3/src/clock.v
@@ -8,7 +8,14 @@ module clock(
 	output reg 4Hz_clk
 );
 
-integer 2Hz_cnt, 1Hz_cnt, 200Hz_cnt; always @ (posedge clk) begin // clk is 100 MHz or 100,000,000 if (rst) begin 2Hz_cnt 	= 0; 1Hz_cnt 	= 0; 100Hz_cnt 	= 0;
+integer 2Hz_cnt, 1Hz_cnt, 200Hz_cnt, 4Hz_cnt;
+
+// clk is 100 MHz or 100,000,000
+always @ (posedge clk) begin
+	if (rst) begin
+		2Hz_cnt 	= 0;
+		1Hz_cnt 	= 0;
+		100Hz_cnt 	= 0;
 		4Hz_cnt 	= 0;
 
 		2Hz_clk 	<= 0;
@@ -24,7 +31,7 @@ integer 2Hz_cnt, 1Hz_cnt, 200Hz_cnt; always @ (posedge clk) begin // clk is 100 
 
 
 		// Manage 2 Hz clk
-		if (2Hz_cnt == 50,000,000)	begin
+		if (2Hz_cnt == 50,000,000 - 1)	begin
 			2Hz_clk <= 1;
 			2Hz_cnt <= 0;
 		end
@@ -33,7 +40,7 @@ integer 2Hz_cnt, 1Hz_cnt, 200Hz_cnt; always @ (posedge clk) begin // clk is 100 
 		end
 
 		// Manage 1 Hz clk
-		if (1Hz_cnt == 100,000,000)	begin
+		if (1Hz_cnt == 100,000,000 - 1)	begin
 			1Hz_clk <= 1;
 			1Hz_cnt <= 0;
 		end
@@ -42,7 +49,7 @@ integer 2Hz_cnt, 1Hz_cnt, 200Hz_cnt; always @ (posedge clk) begin // clk is 100 
 		end
 
 		// Manage 100 Hz clk
-		if (100Hz_cnt == 1,000,000)	begin
+		if (100Hz_cnt == 1,000,000 - 1)	begin
 			100Hz_clk <= 1;
 			100Hz_cnt <= 0;
 		end
@@ -51,7 +58,7 @@ integer 2Hz_cnt, 1Hz_cnt, 200Hz_cnt; always @ (posedge clk) begin // clk is 100 
 		end
 
 		// Manage 4 Hz clk
-		if (4Hz_cnt == 25,000,000)	begin
+		if (4Hz_cnt == 25,000,000 - 1)	begin
 			4Hz_clk <= 1;
 			4Hz_cnt <= 0;
 		end
