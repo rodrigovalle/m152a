57aea54f3e34c0f8ed111767ba7654288e690a06
rstenberg <ryanstenberg10@gmail.com>
2017-02-16 15:43:17 -0800

    Stopwatch, added counters for minutes/seconds

--- lab3/src/stopwatch.v
+++ lab3/src/stopwatch.v
@@ -9,4 +9,40 @@ module stopwatch(
 	output 	[3:0]	an		// Anodes for diplaying segments
 	);
 
+	wire 1Hz, 2Hz, 4Hz, 200Hz;
+
+	clock_div cdiv(
+        // inputs
+        .clk(clk),
+        .rst(btnR),
+        .pause(btnS),
+
+        // outputs
+        .1Hz_clk(1Hz),
+        .2Hz_clk(2Hz),
+        .4Hz_clk(4Hz),
+        .200Hz_clk(200Hz)
+    	)
+
+	reg [3:0] ones_sec, tens_sec, ones_min, tens_min;
+	wire sec_overflow;
+
+    counter seconds(
+    	.clk(1Hz),
+    	.rst(btnR),
+    	.count_ones(ones_sec),
+    	.count_tens(tens_sec),
+    	.c_out(sec_overflow)
+    	)
+
+    counter minutes(
+    	.clk(sec_overflow),
+    	.rst(btnR),
+    	.count_ones(ones_min),
+    	.count_tens(tens_min),
+    	.c_out()
+    	)
+
+    
+
 endmodule
