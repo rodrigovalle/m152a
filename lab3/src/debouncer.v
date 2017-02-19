301d67fa20e5469680a33c36d0b48fe32341078c
rstenberg <ryanstenberg10@gmail.com>
2017-02-18 20:31:28 -0800

    Debouncer implementation

--- lab3/src/debouncer.v
+++ lab3/src/debouncer.v
@@ -1,2 +1,24 @@
-module deboucner(
+module debouncer(
+		input clk,
+		input rst,
+		input btn_in,
+		output reg btn_vld
 );
+	
+	reg [2:0] hist = 0;
+
+	always @ (posedge clk)
+     if (rst) begin
+     		hist <= 0;
+        	btn_vld <= 1'b0;
+        end
+     else if (clk)
+       begin
+       		hist <= {hist[1:0], btn_in};
+        	if (hist == 2)
+        		btn_vld <= 1;
+        	else
+        		btn_vld <= 0;
+       end
+
+endmodule
