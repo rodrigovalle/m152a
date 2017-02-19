39bf067566c189819b1237c6cee9c6c07d77b4dd
Rodrigo Valle <rdan.valle@gmail.com>
2017-02-18 20:37:41 -0800

    stopwatch now uses debouncer

--- lab3/src/stopwatch.v
+++ lab3/src/stopwatch.v
@@ -1,20 +1,35 @@
 `timescale 1ns / 1ps
 
 module stopwatch(
-	input 			clk,
-	input	[7:0] 	sw, 	// sw[0] = adj; sw[1] = sel;
-	input 			btnS,	// pause
-	input 			btnR, 	// rst
-	output	[7:0]	seg,	// Seven segments + decimal
-	output 	[3:0]	an		// Anodes for diplaying segments
-	);
+    input           clk,
+    input   [1:0]   sw,     // sw[0] = adj; sw[1] = sel;
+    input           btnS,   // pause
+    input           btnR,   // rst
+    output  [7:0]   seg,    // Seven segments + decimal
+    output  [3:0]   an      // Anodes for diplaying segments
+    );
 
-	wire 1Hz, 2Hz, 4Hz, 200Hz;
+    wire 1Hz, 2Hz, 4Hz, 200Hz;
+    wire rst, pause;
 
-	clock_div cdiv(
+    debouncer rst_db(
+        .clk(200Hz),
+        .rst(),
+        .btn_in(btnR),
+        .btn_vld(rst)
+    )
+
+    debouncer pause_db(
+        .clk(200Hz),
+        .rst(),
+        .btn_in(btnS),
+        .btn_vld(pause)
+    )
+
+    clock_div cdiv(
         // inputs
         .clk(clk),
-        .rst(btnR),
+        .rst(rst),
         .pause(btnS),
 
         // outputs
@@ -22,26 +37,26 @@ module stopwatch(
         .2Hz_clk(2Hz),
         .4Hz_clk(4Hz),
         .200Hz_clk(200Hz)
-    	)
+    )
 
-	reg [3:0] ones_sec, tens_sec, ones_min, tens_min;
-	wire sec_overflow;
+    reg [3:0] ones_sec, tens_sec, ones_min, tens_min;
+    wire sec_overflow;
 
     counter seconds(
-    	.clk(1Hz),
-    	.rst(btnR),
-    	.count_ones(ones_sec),
-    	.count_tens(tens_sec),
-    	.c_out(sec_overflow)
-    	)
+        .clk(1Hz),
+        .rst(rst),
+        .count_ones(ones_sec),
+        .count_tens(tens_sec),
+        .c_out(sec_overflow)
+    )
 
     counter minutes(
-    	.clk(sec_overflow),
-    	.rst(btnR),
-    	.count_ones(ones_min),
-    	.count_tens(tens_min),
-    	.c_out()
-    	)
+        .clk(sec_overflow),
+        .rst(rst),
+        .count_ones(ones_min),
+        .count_tens(tens_min),
+        .c_out()
+    )
 
     
 
